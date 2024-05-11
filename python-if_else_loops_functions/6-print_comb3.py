e96ac624e669539e36573e81e5d1e459e3512d17
Magnusmajo <alexisoblivion@gmail.com>
2024-05-11 01:42:24 +0000

    different combinations of two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-for x in range(10):
-    for y in range(x + 1, 10):
-        print("{:02d},".format(x * 10 + y), end=", ")
-
+for i in range(10):
+    for j in range(i + 1, 10):
+        if 1 <= 8 and j <= 9:
+            if i == 8 and j == 9:
+                print("{}{}".format(i, j))
+            else:
+                print("{}{}, ".format(i, j), end="")
+        else:
+            print("{}{} ".format(i, j), end=", ")
