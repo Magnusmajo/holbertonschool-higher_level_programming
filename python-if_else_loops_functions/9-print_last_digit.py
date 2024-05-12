50a76abf921b3b2142dda2515cc62abafbd39966
Magnusmajo <alexisoblivion@gmail.com>
2024-05-12 03:00:12 +0000

    last_digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
-def print_last_digit(num):
-    last_digit = abs(num) % 10
-    print(last_digit, end="")
+def print_last_digit(number):
+    positive_number = abs(number)
+    last_digit = positive_number % 10
+    print(last_digit, end='')
+    return(last_digit % 10)
