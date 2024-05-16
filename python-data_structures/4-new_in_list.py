362cbe46d59571b04a7dc06f215781273a4f16c1
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-16 10:36:28 -0300

    Replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,13 +2,16 @@
 
 def new_in_list(my_list, idx, element):
     """deletes the element at index idx in a list of integers and replaces it"""
+    kopiereen = my_list
+    kopiereen[idx] = element
     if idx < 0 or idx >= len(my_list):
         return my_list.copy
-    kopiereen = my_list.copy()
-    kopiereen[idx] = element
     return kopiereen
 
 my_list = [1, 2, 3, 4, 5]
 idx = 3
 new_element = 9
 new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
