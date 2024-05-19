77fa101c8de3b27250a1ebd2897da8b61d23f1cd
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-18 23:40:58 -0300

    Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    getal = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
+            getal += 1
     except IndexError:
-        break
-    finally:
-        print("")
-    return min(x, len(my_list))
+        pass
+    print("")
+    return getal
