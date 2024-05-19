178d334cea7cf03867ba36f0cf96907167eabcbf
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-19 00:01:05 -0300

    Integers division with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    resultaat = None
     try:
         resultaat = a / b
     except ZeroDivisionError:
-        resultaat = None
+        pass
     finally:
         print("Inside resultaat: {}".format(result))
     return resultaat
