4b40bad2386f9974fa5522541f4a220fc9442a60
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-19 00:02:11 -0300

    Integers division with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         pass
     finally:
-        print("Inside resultaat: {}".format(result))
+        print("Inside resultaat: {}".format(resultaat))
     return resultaat
