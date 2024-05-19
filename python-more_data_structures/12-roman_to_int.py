c181517bf243925f3a3de2ecca415cf35b2cea6b
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-18 23:19:45 -0300

    Roman to Integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+    
+    rom_val = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    totaal = 0
+    vorige_waarde = 0
+    
+    for char in reversed(roman_string):
+        waarde = rom_val[char]
+        if waarde >= vorige_waarde:
+            totaal += waarde
+        else:
+            totaal -= waarde
+        vorige_waarde = waarde
+    return totaal
