e7ea7fb05d90a3a63e3294471a98c88198ef198c
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-19 13:57:35 -0300

    Safe printing of an integers list

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
-def safe_print_list_integers(my_list=[], x=0):
+def safe_print_list_integers(my_list=None, x=None):
+    if my_list is None or x is None:
+        return 0
     tellen = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                tellen += 1
-    except IndexError:
-        pass
-    finally:
-        print()
-        return tellen
+    for i in range(min(x, len(my_list))):
+        if isinstance(my_list[i], int):
+            print("{:d}".format(my_list[i]), end="")
+            tellen += 1
+    print()
+    return tellen
