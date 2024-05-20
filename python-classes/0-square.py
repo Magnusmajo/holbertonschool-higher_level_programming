f928147cf1725fe4257c1b4615bebba537532c74
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-19 22:40:15 -0300

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
+# This is a Python class named 'Square' that represents a square shape.
 class Square:
+# This is a class attribute that represents the number of instances created.
     pass
