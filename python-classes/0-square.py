65b633c86213a6826a67966035f1a0415e6e7a79
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-19 22:42:31 -0300

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
-# This is a Python class named 'Square' that represents a square shape.
+"""
+This module contains the class BaseGeometry a Python class named 'Square' that represents a square shape.
+"""
 class Square:
-# This is a class attribute that represents the number of instances created.
+    """
+    # This is a class attribute that represents the number of instances created.
+    pass
+    """
     pass
