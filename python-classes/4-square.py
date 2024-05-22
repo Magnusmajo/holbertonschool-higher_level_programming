8993d31cb3eca5091075395a27319b4444c83e4b
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-22 14:23:06 -0300

    Printing a square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -45,3 +45,14 @@ class Square:
             int: Area of square
         """
         return self.__size ** 2
+    
+    def my_print(self):
+        """
+        Prints the square with the character #
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
