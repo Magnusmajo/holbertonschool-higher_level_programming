513e9a36abe36ce6f6ea4151a361ca4d5888f081
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-22 14:32:31 -0300

    Printing a square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -59,5 +59,4 @@ class Square:
             print("")
         else:
             for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
+                print("#"* self.__size,  end="")
