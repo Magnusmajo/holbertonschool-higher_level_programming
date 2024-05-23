8d0ceeec34d64c50b72b9d1abc1885fa7af1fb7c
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-23 11:56:06 -0300

    Simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+    """
+    Rectangle:
+    A class that defines a rectangle
+    """
+    
+    
+    class Rectangle:
+        """
+        A class that defines a rectangle
+        """
