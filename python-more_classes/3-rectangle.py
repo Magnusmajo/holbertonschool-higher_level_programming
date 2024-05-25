c4d4979ff04d83d134d03ae0c82a7aab14a7bfd6
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-25 11:57:38 -0300

    String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -86,7 +86,8 @@ class Rectangle:
         result = ""
         for i in range(self._height):
             if i == self._height - 1:
-                result += "#" * self._width + "\n"
+                result.append("#" * self.__width)
             else:
-                result += "#" * self._width
+                result.append(("#" * self.__width) + "\n")
+                
         return "".join(result)
