f8c285018a64ef4836978ecd9ddb45f4540a08e8
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-25 22:06:38 -0300

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,18 +4,9 @@
 """
 
 
-
 def lookup(obj):
     """
     This function takes an object as input and returns a dictionary with the object's attributes as keys and their corresponding values as values.
     """
     list = obj
     return list(dir)
-
-class MyClass1(object):
-    pass
-
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
