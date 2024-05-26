cae07520601475f2e19e09968ecaeb28bb90984b
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-25 23:05:19 -0300

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+BaseGeometry:
+- public instance method called area.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+
+    def area(self):
+        """
+        Raises a NotImplementedError
+        """
+        raise Exception('area() is not implemented')
