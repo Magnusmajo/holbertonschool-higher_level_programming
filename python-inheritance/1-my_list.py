a01627ebde7488025b062323cade69a2c3ff5115
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-25 22:21:09 -0300

    My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+MyList:
+    A class called MyList
+"""
+
+
+class MyList:
+    """
+    A class that inherits from list
+    """
+    def print_sorted(self):
+        """
+        Prints a sorted list
+        """
+        print(sorted(self))
