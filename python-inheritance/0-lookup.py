b1ef50790942bfdf90b7428bfc0c025a5326b067
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-26 21:17:00 -0300

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,6 +6,8 @@ A function that returns the list of options
 
 def lookup(obj):
     """
-    This function takes an object as input and returns a dictionary with the object's attributes as keys and their corresponding values as values.
+    This function takes an object as input
+    and returns a dictionary with the object's
+    attributes as keys and their corresponding values as values.
     """
     return dir(obj)
