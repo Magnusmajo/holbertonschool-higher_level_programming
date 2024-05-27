c3d2baa9940f24a5f86c440d07c974cdeb83452c
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-26 22:43:00 -0300

    Square #2"

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+BaseGeometry:
+- public instance method called area.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A Square is a Rectangle with equal width and height.
+    Arg:
+    size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a Square.
+        Args:
+            size (_type_): _description_
+        """
+        if self.integer_validator("size", size):
+            self.__size = size
+
+    def area(self):
+        """
+        Defines the area of the square
+        Returns:
+        int: The area of the square
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Returns a string representation of the square
+        """
+        return f"[Square] {self.__size}/{self.__size}"
