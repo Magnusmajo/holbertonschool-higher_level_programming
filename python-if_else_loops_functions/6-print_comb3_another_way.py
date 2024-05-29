daef790a14caef2302f221312ba49c75830d8d88
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-29 12:52:38 -0300

    combinations of two numbers, just another way

--- python-if_else_loops_functions/6-print_comb3_another_way.py
+++ python-if_else_loops_functions/6-print_comb3_another_way.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""
-program that prints all possible different combinations of two digits.
-Numbers must be separated by ,, followed by a space
-The two digits must be different
-01 and 10 are considered the same combination of the two digits 0 and 1
-Print only the smallest combination of two digits
-Numbers should be printed in ascending order, with two digits
+"""A program that prints all possible different combinations of two digits.
+
+This program generates and prints all unique combinations of two digits, where
+the digits are separated by a comma and a space. The combinations are printed
+in ascending order, and only the smallest combination of two digits is printed.
+
+The program uses the join function to concatenate a sequence of strings, which
+are generated using a generator expression. The generator expression uses the
+range function to generate numbers from 0 to 9, and f-string formatting is used
+to convert each number to a two-digit string.
+
+The program excludes the combination "89" from the output, as it is considered
+the same as "98".
 """
 
-print(', '.join(f"{i}{j}" if not (i == 8 and j == 9) else f"{i}{j}\n" \
+print(', '.join(f"{i}{j}" if not (i == 8 and j == 9) else f"{i}{j}\n"
     for i in range(10) for j in range(i + 1, 10)))
-"""
-Use the join function to concatenate a sequence of strings
-The sequence is generated using a generator expression
-The generator expression uses the range function to generate numbers from 0 to 99
-The f-string formatting is used to convert each number to a two-digit string
-"""
