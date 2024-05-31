d767f6f2352aebeb6e0a0351fb55b791583a23bf
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-31 20:37:54 -0300

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,26 +4,15 @@ Load, add, save
 """
 import json
 import sys
-import os
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    if os.path.exists('add_item.json'):
-        with open('add_item.json', 'r') as f:
-            add_arg = json.load(f)
-    else:
-        add_arg = []
-except json.JSONDecodeError as e:
-    sys.exit(1)
+    arguments = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    arguments = []
 
-for i in arg:
-    if i not in add_arg:
-        add_arg.append(i)
-
-try:
-    with open('add_item.json', 'w') as f:
-        json.dump(add_arg, f)
-except Exception as e:
-    sys.exit(1)
+arguments.extend(sys.argv[1:])
+save_to_json_file(arguments, 'add_item.json')
