1c172b87c931251be058be940a914724c650590a
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-31 15:34:52 -0300

    From JSON string to Object

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -8,9 +8,10 @@ import json
 def from_json_string(my_str):
     """Convert an object to a JSON string.
 
-    This function takes an object as input and returns a JSON string representation
-    of the object. The function uses the json module's dumps function to convert
-    the object to a JSON string.
+    This function takes an object as input and returns
+    a JSON string representation of the object.
+    The function uses the json module's dumps function
+    to convert the object to a JSON string.
 
     Args:
         my_obj (object): The object to convert to a JSON string.
