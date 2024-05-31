d563a9924311b340448a4ec6eee38544ee73553c
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-31 20:35:58 -0300

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Load, add, save
 """
 import json
 import sys
-
+import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -13,11 +13,20 @@ arg = sys.argv[1:]
 add_arg = []
 
 try:
-    add_arg = load_from_json_file('add_item.json')
-    for i in arg:
+    if os.path.exists('add_item.json'):
+        with open('add_item.json', 'r') as f:
+            add_arg = json.load(f)
+    else:
+        add_arg = []
+except json.JSONDecodeError as e:
+    sys.exit(1)
+
+for i in arg:
+    if i not in add_arg:
         add_arg.append(i)
-        save_to_json_file(add_arg, 'add_item.json')
-except FileNotFoundError:
-    for i in arg:
-        add_arg.append(i)
-        save_to_json_file(add_arg, 'add_item.json')
+
+try:
+    with open('add_item.json', 'w') as f:
+        json.dump(add_arg, f)
+except Exception as e:
+    sys.exit(1)
