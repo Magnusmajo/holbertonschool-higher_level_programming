9fd270af3b47209f32acbe1ef9c13b330f79dc56
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-31 20:22:04 -0300

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,14 +2,17 @@
 """
 Load, add, save
 """
-import json
 import sys
+from os import path
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    filename = "add_item.json"
+filename = "add_item.json"
+if path.exists(filename):
     my_list = load_from_json_file(filename)
-    my_list.extend(sys.argv[1:])
-    save_to_json_file(my_list, filename)
+else:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+save_to_json_file(my_list, filename)
