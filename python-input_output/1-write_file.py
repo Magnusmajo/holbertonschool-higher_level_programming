49e7f1424e7b5cd305ef81ecb659c866dbc6964c
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-05-31 15:09:21 -0300

    Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""
+Writes a string to a text file and return the
+numbers of characters written
+"""
+
+def write_file(filename="", text=""):
+    """
+        Writes a string to a text file and return the
+        numbers of characters written
+        """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
