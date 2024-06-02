cb474c5652adb0e3a75b7011ad402ebb24c854c7
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-06-01 23:01:24 -0300

    Pickling Custom Classes

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Pickling Custom Classes
+"""
+import pickle  
+
+
+class CustomObject:
+    """This class represents a custom object with name, age,
+    and is_student attributes."""
+
+    def __init__(self, name, age, is_student):
+        """Initializing the object with the given name, age,
+        and is_student values."""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Displaying the name, age, and is_student attributes
+        of the object."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serializing the object and saving it to a file."""
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except Exception as e:
+            """Printing an error message if serialization fails."""
+            print(f"An error occurred while serializing: {e}")
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserializing the object from a file."""
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except Exception as e:
+            """Printing an error message if deserialization fails."""
+            print(f"An error occurred while deserializing: {e}")
+            return None
