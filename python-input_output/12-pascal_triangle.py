17b91a642ef084ac0c30772b972c1b9700651052
Alexis <159050514+Magnusmajo@users.noreply.github.com>
2024-06-01 22:24:03 -0300

    Pascal's Triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
+
 
 def pascal_triangle(n):
     """Returns a list of lists of integers representing
@@ -6,7 +10,7 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-
+    
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
@@ -15,5 +19,5 @@ def pascal_triangle(n):
                 for j in range(len(last_row) - 1)]
         row.append(1)
         triangle.append(row)
-
+    
     return triangle
