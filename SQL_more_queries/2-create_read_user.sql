4bb06197a544da862177fcbdad3f2a14c6c07efe
Magnusmajo <alexisoblivion@gmail.com>
2024-06-22 22:04:53 +0000

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 -- Create the database
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE OR REPLACE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
