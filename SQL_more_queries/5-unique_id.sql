294efc98f233b9681252e71553e0fdbd32946a7c
Magnusmajo <alexisoblivion@gmail.com>
2024-06-22 22:20:32 +0000

    Unique id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
 -- in database
 -- Create another table
 
-CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT=1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT= 1 UNIQUE, name VARCHAR(256));
