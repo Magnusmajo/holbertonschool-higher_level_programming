77d9ffcb241cf4d66d62ca91a4a5dc791e05fad2
Magnusmajo <alexisoblivion@gmail.com>
2024-06-23 03:40:26 +0000

    States table

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 -- Create another database with another table
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(256) NOT NULL
 );
