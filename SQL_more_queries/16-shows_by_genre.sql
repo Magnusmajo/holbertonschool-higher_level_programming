f2c6a514d3ff9a3096aa05ee6ab8a21726b3de57
Magnusmajo <alexisoblivion@gmail.com>
2024-06-23 04:59:01 +0000

    List shows and genres

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -5,4 +5,4 @@ SELECT tv_shows.title, tv_genres.name
 FROM tv_genres
 RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN tv_shows ON tv_show.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_genres;
+ORDER BY tv_shows.title, tv_genres.name;
