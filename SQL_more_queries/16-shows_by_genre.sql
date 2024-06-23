8981deb0631861ef6910f198b60bb8710497eced
Magnusmajo <alexisoblivion@gmail.com>
2024-06-23 04:53:03 +0000

    List shows and genres

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- in database
 -- dump dump
 
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre_name
-FROM tv_shows
+SELECT tv_shows.title, tv_genres.name
+FROM tv_genres
 RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre_name ASC;
+ORDER BY tv_shows.title ASC, tv_genres ASC
