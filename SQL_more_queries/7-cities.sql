8c9f0fde3e6aa02c5c2afb0ec93bef27fa9b37f8
Magnusmajo <alexisoblivion@gmail.com>
2024-06-23 04:00:52 +0000

    Cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,9 +2,8 @@
 -- Create database and table
 
 CREATE DATABASE hbtn_0d_usa;
-USE hbtn_0d_usa;
 
-CREATE TABLE cities(
+CREATE TABLE hbtn_0d_usa.cities(
     'id' INT NOT NULL AUTO_INCREMENT UNIQUE,
     'state_id' INT NOT NULL,
     'name' VARCHAR(256) NOT NULL,
