10093255d383620b0a978d2d5d4757b8ed2460c5
Magnusmajo <alexisoblivion@gmail.com>
2024-06-24 01:19:42 +0000

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,25 +7,26 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Getting command line arguments
-    def states(username, password, db_name):
-        # Connect to MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306,
-                             user=username, passwd=password, db=db_name)
-        
-        # Creat a cursor object
-        cursor = db.cursor()
-        
-        # Execute the query
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        
-        # Fetch all results
-        results = cursor.fetchall()
-        
-        # Display results
-        for row in results:
-            print(row)
-            
-            # Close cursor and database connection
-            cursor.close()
-            db.close()
+    # Get command line arguments
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Display results
+    for row in results:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
