e59f277333e0f60e9fb6afe954eaccac465a7c27
Magnusmajo <alexisoblivion@gmail.com>
2024-07-28 23:03:36 -0300

    List of elements

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const addButton = document.getElementById('add_item');
+  const myList = document.querySelector('.my_list');
+
+  addButton.addEventListener('click', function () {
+    const newItem = document.createElement('li');
+    newItem.textContent = 'Item';
+    myList.appendChild(newItem);
+  });
+});
