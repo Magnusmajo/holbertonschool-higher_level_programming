dd259e26796e6cde568e1d0939c354e7cd67ce53
Magnusmajo <alexisoblivion@gmail.com>
2024-08-03 23:15:29 -0300

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,16 +1,39 @@
 #!/usr/bin/python3
-"""Write a script that lists all cities from the database hbtn_0e_4_usa"""
-import sys
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
+import sys
+
+
+def states(username, userPassword, database_name, state_name):
+    """
+    Connects to a MySQL database and executes a query to retrieve all states.
+    """
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                        user=username, passwd=userPassword,
+                        db=database_name)
+
+    cur = db.cursor()
+
+    query = ("SELECT cities.name FROM cities " +
+            "JOIN states ON cities.state_id = states.id " +
+            "WHERE states.name LIKE %s ORDER BY cities.id")
+
+    cur.execute(query, (state_name,))
+
+    rows = cur.fetchall()
+
+    print(", ".join(a[0] for a in rows))
+
+    cur.close()
+
+    db.close()
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
-    c.close()
-    db.close()
+    user = sys.argv[1]
+    passw = sys.argv[2]
+    bd = sys.argv[3]
+    name = sys.argv[4]
+
+    states(user, passw, bd, name)
