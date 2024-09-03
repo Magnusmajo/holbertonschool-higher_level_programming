4b8922db16c61529afb3989bc8649557ed3f3b4f
Magnusmajo <alexisoblivion@gmail.com>
2024-09-02 23:09:11 -0300

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 for i in range(97,123):
     print("{:c}".format(i), end="")
-
-"""
-A more elegant way:
-print(*(chr(i) for i in range(97,123)), sep="")
-
-- * (unpacking operator): The asterisk before the generator expression unpacks the elements of the generated sequence, passing each character as a separate argument to the print function.
-- sep="": This argument of the print function specifies that the elements should be printed without any separator between them. By default, print separates elements with a space, but using sep="" ensures that the characters are printed one after another without spaces.
-- chr(i) for i in range(97,123): This generator expression generates characters corresponding to ASCII values in the range 97 to 122, which represent lowercase letters 'a' to 'z'."""
