b3b502678633858a1450a22d9f578b0af8843ab8
Magnusmajo <alexisoblivion@gmail.com>
2024-09-04 14:03:26 -0300

    00...99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 99:
-        print("{:02}".format(i), end=", ")
-    else:
-        print(f"{i} \n")
+print(', '.join(f"{i:02d}" for i in range(100)))
