10cd2ef219bd8846941fbd02f61699afd2a30be2
Magnusmajo <alexisoblivion@gmail.com>
2024-09-05 16:16:35 -0300

    Everything can be imported

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,14 @@
+import os
+import hidden_4
+
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import hidden_4
 
     alles = dir(hidden_4)
     for name in alles:
         if name[:2] != "__":
             print(name)
+
+# Change the permissions of the script
+os.chmod("/tmp/4-hidden_discovery.py", 0o755)
