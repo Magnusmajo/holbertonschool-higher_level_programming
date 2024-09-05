b56c53e9832e78bc6675d8c2ed0a385e971c8a91
Magnusmajo <alexisoblivion@gmail.com>
2024-09-04 21:47:21 -0300

    Import a simple function from a simple file

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    for i in range(97, 123):
-        print("{:c}".format(i), end='')
+for i in range(97, 123):
+    print("{:c}".format(i), end='')
