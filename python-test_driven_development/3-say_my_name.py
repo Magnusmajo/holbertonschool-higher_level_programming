3850ac906107d21ff43dc1f2f01b9a6ecdf4cbc5
Magnusmajo <alexisoblivion@gmail.com>
2024-09-15 21:39:38 -0300

    Say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,6 +7,7 @@ def say_my_name(first_name, last_name=""):
     """
         Print a person's full name
     """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
