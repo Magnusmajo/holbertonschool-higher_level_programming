89f9e4069e985ed4cd706581127f8a25bfca2e42
Magnusmajo <alexisoblivion@gmail.com>
2024-09-15 21:46:44 -0300

    Text indentation

---
+++ python-test_driven_development/.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""take max integer"""
+
+
+def max_integer(list=[]):
+    """
+    Compares list and returns max integer
+    """
+    if not list:
+        return None
+    result = list[0]
+    for i in range(len(list)):
+        if list[i] > result:
+            result = list[i]
+    return result
