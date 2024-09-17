443c819dc0add5ef6b7ca483b539678e2f232477
Magnusmajo <alexisoblivion@gmail.com>
2024-09-16 23:53:05 -0300

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,9 +5,11 @@
 class Square:
     """A class named Square that defines a square."""
     def __init__(self, size=0):
-        """Initializes the Square instance."""
+        """Initializes the Square instance.
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
-        """The private size of the square."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
