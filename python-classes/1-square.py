f389b65e1e6cd1f83c3b1a85276716a87c819eab
Magnusmajo <alexisoblivion@gmail.com>
2024-09-16 23:36:55 -0300

    Changes

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""This module defines a class Square."""
+"""Module for Square class."""
 
 
 class Square:
-    """A simple class named Square that defines a square."""
+    """A class named Square that defines a square."""
     def __init__(self, size):
         """Initializes the Square instance."""
         self.__size = size
-        """The size of the square."""
+        """The private size of the square."""
