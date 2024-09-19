bbe685ffafdda56e09049400df7aab86fd5ce09f
Magnusmajo <alexisoblivion@gmail.com>
2024-09-19 16:40:24 -0300

    How many instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,11 +4,13 @@
 
 class Rectangle:
     """Class that defines a rectangle"""
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initializate a rectangle instance"""
         self.__height = height
         self.__width = width
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
