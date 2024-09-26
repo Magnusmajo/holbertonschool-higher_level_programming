23823cc151af43680d3723ef9a3b03039f48e95b
Magnusmajo <alexisoblivion@gmail.com>
2024-09-26 15:34:13 -0300

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """An empty class named BaseGeometry"""
+    pass
+
+    def area(self):
+        """Public instance method that Calculates the area of the Geometry.
+        Raises:
+            Exception: If the method has not been implemented.
+        """
+        raise Exception("area() is not implemented")
