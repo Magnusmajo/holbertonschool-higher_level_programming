ebf1ce9df51fc834c256d312a5ad3a6ccbb92604
Magnusmajo <alexisoblivion@gmail.com>
2024-09-25 22:15:26 -0300

    Lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A function that returns the list of options
+"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object
+    """
+    return dir(obj)
