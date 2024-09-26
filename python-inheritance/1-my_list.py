959bf41ee2ed2677a46916e952e41500e0514198
Magnusmajo <alexisoblivion@gmail.com>
2024-09-25 22:28:14 -0300

    My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """A class that inherits from list
+    Arg: 'list': The parent class
+    """
+    def print_sorted(self):
+        """
+        Prints the list, but sorted (ascending sort)
+        """
+        print(sorted(self))
