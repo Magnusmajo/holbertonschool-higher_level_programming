d5163ebd2303475379fec42ae1095978b028c9b6
Magnusmajo <alexisoblivion@gmail.com>
2024-10-05 00:01:12 -0300

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Add item module
-load, add, save
 """
+Load, add, save
+."""
 import sys
 
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_jason_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 
 try:
     arg = load_from_json_file("add_item.json")
