0196b7607a80e9b4591441f607a1d4eeba6518b5
Magnusmajo <alexisoblivion@gmail.com>
2024-10-04 23:58:33 -0300

    Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pythonn3
+"""Add item module"""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_jason_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    arg = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    arg = []
+    arg.extend(sys.argv[1:])
+    save_to_json_file(arg, "add_item.json")
