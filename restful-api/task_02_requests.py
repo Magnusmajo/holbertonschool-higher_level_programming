cba5c55910cf140b49ac5a761145493fae31b5d5
Magnusmajo <alexisoblivion@gmail.com>
2024-10-14 16:38:21 -0300

    Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
+"""api requests"""
 import requests
 import csv
 
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com'
+def fetch_and_print_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code: {response.status_code}")
 
-    def fetch_and_print_posts():
-        response = requests.get(f'{url}/posts')
-        if response.status_code == 200:
-            posts = response.json()
-            for post in posts:
-                print(post['title'])
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(f"Title: {post['title']}")
 
-    def fetch_and_save_posts():
-        response = requests.get(f'{url}/posts')
-        if response.status_code == 200:
-            posts = response.json()
-            with open('posts.csv', 'w') as file:
-                writer = csv.DictWriter(file, fieldnames=['userId', 'id', 'title', 'body'])
-                writer.writeheader()
-                for post in posts:
-                    writer.writerow(post)
+
+def fetch_and_save_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+    if response.status_code == 200:
+        posts = response.json()
+        postsData = [{"id": post["id"], "title": post["title"],
+                        "body": post["body"]} for post in posts]
+
+        with open("posts.csv", 'w') as csvFile:
+            keys = ["id", "title", "body"]
+            dicti = csv.DictWriter(csvFile, fieldnames=keys)
+
+            dicti.writeheader()
+            for post in postsData:
+                dicti.writerow(post)
