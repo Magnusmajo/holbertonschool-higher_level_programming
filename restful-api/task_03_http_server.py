c1e223a9365c9533b22106ba7f33f7ae61698953
Magnusmajo <alexisoblivion@gmail.com>
2024-10-20 21:50:50 -0300

    Develop a simple API using Python with the `http.server` module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -30,16 +30,15 @@ class CustomRequestHandler(http.server.BaseHTTPRequestHandler):
 
         elif self.path == '/status':
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(json.dumps(
-                {"status": "OK"}).encode())
+            self.wfile.write(b'OK')
 
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'Endpoint not found')
+            self.wfile.write(b'404 Not Found')
 
 
 def run(server_class=http.server.HTTPServer,
@@ -51,5 +50,5 @@ def run(server_class=http.server.HTTPServer,
     except KeyboardInterrupt:
         pass
 
-if __name__ == '__main__':
-    run()
+
+run()
