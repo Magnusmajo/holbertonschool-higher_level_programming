3fb916f7736a5cd2b472932ba36eb7e636c757fa
Magnusmajo <alexisoblivion@gmail.com>
2024-12-04 21:03:33 -0300

    And with a list, is it really the same

--- python-everything_is_object/pruebas.py
+++ python-everything_is_object/pruebas.py
@@ -2,12 +2,12 @@ from io import open
 
 # answer = 'yes'
 # answer = 'no'
-answer = 'True'
-# answer = 'False'
+# answer = 'True'
+answer = 'False'
 
 with open('13-answer.txt', 'w') as f:
     f.write(answer)
 
 l1 = [1, 2, 3]
 l2 = l1
-print(l1 == l2)
+print(l1 is l2)
