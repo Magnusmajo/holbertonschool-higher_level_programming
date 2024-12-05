8ced846577fb77f3b6c47703931e968dbbf439a9
Magnusmajo <alexisoblivion@gmail.com>
2024-12-04 21:05:13 -0300

    List append

--- python-everything_is_object/pruebas.py
+++ python-everything_is_object/pruebas.py
@@ -1,13 +1,14 @@
 from io import open
 
-# answer = 'yes'
+answer = '[1, 2, 3, 4]'
 # answer = 'no'
 # answer = 'True'
-answer = 'False'
+# answer = 'False'
 
-with open('13-answer.txt', 'w') as f:
+with open('14-answer.txt', 'w') as f:
     f.write(answer)
 
 l1 = [1, 2, 3]
 l2 = l1
-print(l1 is l2)
+l1.append(4)
+print(l2)
